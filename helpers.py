aeb55e635022f37684231f02611be9362029b1dd
adam gluck <adam.gluck10@gmail.com>
2023-09-11 14:59:19 -0400

    refactor

--- helpers.py
+++ helpers.py
@@ -1,3 +1,6 @@
+from collections import Counter
+import string
+
 def prettyPrint(text, columns=10):
     for i, let in enumerate(text.split(' ')):
         if i % columns == 0:
@@ -26,3 +29,50 @@ def replaceLetter(letter, mappings, hide=False):
     if letter not in mappings:
         return letter if (not hide or letter in set([' ', ' \n'])) else '_'
     return mappings[letter]
+
+def getSequences(text, seqLen=3, minFreq=1):
+    rawCipher = text.replace(" ", "")
+    repeats = []
+    seqLen=seqLen-1
+
+    for i in range(len(rawCipher) - seqLen):
+        repeats.append(rawCipher[i:i+seqLen+1])
+
+    freq = Counter(repeats)
+    return {k: count for k, count in freq.items() if count > minFreq}
+
+def getConsecutive(text,minFreq=1):
+    rawCipher = text.replace(" ", "")
+    repeats=[]
+    for doubleLen in range(2,6):
+        for i in range(len(rawCipher) - doubleLen):
+            if all([rawCipher[i] == rawCipher[i+j] for j in range(1, doubleLen)]):
+                repeats.append(rawCipher[i:i+doubleLen])
+    freq = Counter(repeats)
+    return {k: count for k, count in freq.items() if count > minFreq}
+
+
+def decrypt(text, mapping, hide=False):
+    return "".join(list(map(lambda x: replaceLetter(x, mapping, hide=hide), text)))
+
+class Mapping:
+    def __init__(self, mapping={}):
+        self.map = mapping
+        
+    def addLetter(self, cipherLetter, letter):
+        self.map[cipherLetter.upper()] = letter.lower()
+    
+    def addWord(self, cipherWord, word):
+        for cipherLetter, letter in zip(cipherWord, word):
+            self.addLetter(cipherLetter, letter)
+    
+    def getRemaining(self):
+        return set(string.ascii_lowercase) - set(self.map.values())
+        
+    def getKey(self):
+        return sorted(self.map.items(), key=lambda x: x[1])
+    
+    def decrypt(self, text, hide=False, pretty=True):
+        if pretty:
+            prettyPrint(decrypt(text, self.map, hide=hide))
+        return decrypt(text, self.map, hide=hide)
